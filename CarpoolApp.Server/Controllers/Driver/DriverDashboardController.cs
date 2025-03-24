d222e5e2aca3e0662bea185abcae6b7a03bb64f8
muneeeb12 <itsmuneebkhan@gmailcom>
2025-03-25 03:19:15 +0500

    driver, passenger controller added!!

---
+++ CarpoolApp.Server/Controllers/Driver/DriverDashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarpoolApp.Server.Controllers.Driver
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverDashboardController : ControllerBase
+    {
+    }
+}
